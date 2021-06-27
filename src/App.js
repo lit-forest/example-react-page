2aa6984ec454439886e5f1c132892558861cdc4d
chensihan@sys-in.com.cn <chensihan@sys-in.com.cn>
2021-06-27 13:39:35 +0100

    change the .git file

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Testing publish function for react example.
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
